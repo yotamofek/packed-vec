9536ba1c3ca9bda6a389e959eca3992dde13b84c
Yotam Ofek <yotam.ofek@gmail.com>
2021-03-01 19:53:11 +0200

    Use variable lengthed encoding for compression.

--- src/lib.rs
+++ src/lib.rs
@@ -2,86 +2,17 @@
 #![feature(is_sorted, const_generics, const_evaluatable_checked)]
 
 use std::{
-    convert::{TryFrom, TryInto},
     fmt::{self, Debug},
-    marker::PhantomData,
-    mem::size_of,
-    ops,
+    io,
 };
 
-pub use num_traits::Zero;
+use prefix_varint::{read_varint, write_varint};
 
-pub trait NeBytes<const SIZE: usize> {
-    fn from_ne_bytes(bytes: [u8; SIZE]) -> Self;
-    fn into_ne_bytes(self) -> [u8; SIZE];
-}
-
-pub trait Num:
-    Sized
-    + Copy
-    + PartialEq
-    + PartialOrd
-    + ops::Sub<Self, Output = Self>
-    + Zero
-    + NeBytes<{ size_of::<Self>() }>
-where
-    [u8; size_of::<Self>()]: ,
-{
-}
-
-impl<N> Num for N
-where
-    N: Sized
-        + Copy
-        + PartialEq
-        + PartialOrd
-        + ops::Sub<Self, Output = Self>
-        + Zero
-        + NeBytes<{ size_of::<Self>() }>,
-    [u8; size_of::<Self>()]: ,
-{
-}
-
-macro_rules! impl_ne_bytes {
-    ($t:ident) => {
-        impl NeBytes<{ size_of::<Self>() }> for $t {
-            fn from_ne_bytes(bytes: [u8; size_of::<Self>()]) -> Self {
-                Self::from_ne_bytes(bytes)
-            }
-
-            fn into_ne_bytes(self) -> [u8; size_of::<Self>()] {
-                Self::to_ne_bytes(self)
-            }
-        }
-    };
-    ($t: ident $($ts:ident)+) => {
-        impl_ne_bytes!($t);
-        impl_ne_bytes!($($ts)+);
-    };
-}
-
-impl_ne_bytes!(u64 u32 u16 u8);
-
-pub struct PackedVec<V, D>
-where
-    V: Num + From<D>,
-    D: Num + TryFrom<V>,
-    [u8; size_of::<V>()]: ,
-    [u8; size_of::<D>()]: ,
-{
+pub struct PackedVec {
     inner: Vec<u8>,
-
-    _v_marker: PhantomData<V>,
-    _d_marker: PhantomData<D>,
 }
 
-impl<V, D> Debug for PackedVec<V, D>
-where
-    V: Num + From<D> + Debug,
-    D: Num + TryFrom<V>,
-    [u8; size_of::<V>()]: ,
-    [u8; size_of::<D>()]: ,
-{
+impl Debug for PackedVec {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         let vec = self.iter().collect::<Vec<_>>();
 
@@ -89,122 +20,29 @@ where
     }
 }
 
-impl<V, D> Clone for PackedVec<V, D>
-where
-    V: Num + From<D>,
-    D: Num + TryFrom<V>,
-    [u8; size_of::<V>()]: ,
-    [u8; size_of::<D>()]: ,
-{
+impl Clone for PackedVec {
     fn clone(&self) -> Self {
         Self {
             inner: self.inner.clone(),
-            ..Self::new()
         }
     }
 }
 
-impl<V, D> Default for PackedVec<V, D>
-where
-    V: Num + From<D>,
-    D: Num + TryFrom<V>,
-    [u8; size_of::<V>()]: ,
-    [u8; size_of::<D>()]: ,
-{
+impl Default for PackedVec {
     fn default() -> Self {
         Self::new()
     }
 }
 
-enum Delta<V, D> {
-    Small(D),
-    Big(V),
-}
-
-impl<V, D> Delta<V, D>
-where
-    V: Num + From<D>,
-    D: Num + TryFrom<V>,
-    [u8; size_of::<V>()]: ,
-    [u8; size_of::<D>()]: ,
-{
-    fn compress(delta: V) -> Self {
-        if let Ok(small_delta) = D::try_from(delta) {
-            Delta::Small(small_delta)
-        } else {
-            Delta::Big(delta)
-        }
-    }
-
-    fn size_of_val(&self) -> usize {
-        match self {
-            Delta::Small(..) => size_of::<D>(),
-            Delta::Big(..) => size_of::<D>() + size_of::<V>(),
-        }
-    }
-
-    fn as_value(&self) -> V {
-        match *self {
-            Delta::Small(small) => <V as From<D>>::from(small),
-            Delta::Big(value) => value,
-        }
-    }
-
-    fn to_bytes(&self) -> Vec<u8> {
-        match *self {
-            Delta::Small(small) => small.into_ne_bytes().to_vec(),
-            Delta::Big(big) => {
-                let mut bytes = D::into_ne_bytes(D::zero()).to_vec();
-                bytes.extend_from_slice(&V::into_ne_bytes(big));
-                bytes
-            }
-        }
-    }
-}
-
-fn read_delta<V, D>(data: &[u8]) -> Delta<V, D>
-where
-    V: Num + From<D>,
-    D: Num + TryFrom<V>,
-    [u8; size_of::<V>()]: ,
-    [u8; size_of::<D>()]: ,
-{
-    let delta = D::from_ne_bytes(data[0..size_of::<D>()].try_into().unwrap());
-
-    if !delta.is_zero() {
-        Delta::Small(delta)
-    } else {
-        let delta = V::from_ne_bytes(
-            data[size_of::<D>()..size_of::<D>() + size_of::<V>()]
-                .try_into()
-                .unwrap(),
-        );
-
-        Delta::Big(delta)
-    }
-}
-
-impl<V, D> PackedVec<V, D>
-where
-    V: Num + From<D>,
-    D: Num + TryFrom<V>,
-    [u8; size_of::<V>()]: ,
-    [u8; size_of::<D>()]: ,
-{
+impl PackedVec {
     pub fn new() -> Self {
-        Self {
-            inner: Vec::new(),
-            _v_marker: Default::default(),
-            _d_marker: Default::default(),
-        }
+        Self { inner: Vec::new() }
     }
 
-    pub fn iter(&self) -> PackedVecIter<'_, V, D> {
+    pub fn iter(&self) -> PackedVecIter<'_> {
         PackedVecIter {
-            raw: self.inner.as_slice(),
-            cur_val: V::zero(),
-
-            _marker: Default::default(),
+            raw: io::Cursor::new(self.inner.as_slice()),
+            cur_val: 0,
         }
     }
 
@@ -217,37 +55,26 @@ where
     }
 
     #[inline]
-    pub fn push(&mut self, el: V) -> InsertPosition {
-        let mut extender = PackedVecExtender {
-            vec: self,
-            cur_val: V::zero(),
-            pos: 0,
-            idx: 0,
-
-            _v_marker: Default::default(),
-            _d_marker: Default::default(),
-        };
+    pub fn push(&mut self, el: u64) -> InsertPosition {
+        let mut extender = self.extender();
 
         unsafe { extender.push(el) }
     }
 
     #[inline]
-    pub fn extender(&mut self) -> PackedVecExtender<'_, V, D> {
+    pub fn extender(&mut self) -> PackedVecExtender<'_> {
         PackedVecExtender {
             vec: self,
-            cur_val: V::zero(),
+            cur_val: 0,
             pos: 0,
             idx: 0,
-
-            _v_marker: Default::default(),
-            _d_marker: Default::default(),
         }
     }
 
     /// # Safety
     /// Passing a slice that is either not completely sorted or not completely deduped
     /// can corrupt this structure's internal storage
-    pub unsafe fn extend_from_sorted_unique_slice(&mut self, elements: &[V]) {
+    pub unsafe fn extend_from_sorted_unique_slice(&mut self, elements: &[u64]) {
         debug_assert!(elements.is_sorted());
         debug_assert!({
             let cur = elements.iter().skip(1);
@@ -264,20 +91,11 @@ where
     }
 }
 
-pub struct PackedVecExtender<'a, V, D>
-where
-    V: Num + From<D>,
-    D: Num + TryFrom<V>,
-    [u8; size_of::<V>()]: ,
-    [u8; size_of::<D>()]: ,
-{
-    vec: &'a mut PackedVec<V, D>,
-    cur_val: V,
+pub struct PackedVecExtender<'a> {
+    vec: &'a mut PackedVec,
+    cur_val: u64,
     pos: usize,
     idx: usize,
-
-    _v_marker: PhantomData<V>,
-    _d_marker: PhantomData<D>,
 }
 
 #[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash, Debug)]
@@ -294,14 +112,16 @@ impl InsertPosition {
     }
 }
 
-impl<'a, V, D> PackedVecExtender<'a, V, D>
-where
-    V: Num + From<D>,
-    D: Num + TryFrom<V>,
-    [u8; size_of::<V>()]: ,
-    [u8; size_of::<D>()]: ,
-{
-    unsafe fn read_delta(&mut self) -> Option<Delta<V, D>> {
+fn write_varint_to_buf(n: u64) -> io::Result<Vec<u8>> {
+    let mut buf = vec![];
+
+    write_varint(n, &mut buf)?;
+
+    Ok(buf)
+}
+
+impl<'a> PackedVecExtender<'a> {
+    unsafe fn read_delta(&mut self) -> Option<(u64, usize)> {
         let Self {
             vec: PackedVec { ref inner, .. },
             pos,
@@ -314,23 +134,19 @@ where
             return None;
         }
 
-        let delta = read_delta(slice);
+        let mut cursor = io::Cursor::new(slice);
 
-        Some(delta)
-    }
+        let delta = read_varint(&mut cursor).unwrap();
 
-    unsafe fn advance(&mut self, delta: Delta<V, D>) {
-        self.idx += 1;
-        self.pos += delta.size_of_val();
-        self.cur_val = self.cur_val + delta.as_value();
+        Some((delta, cursor.position() as usize))
     }
 
     /// # Safety
     /// Using this function on a set of elements that is either not completely sorted or not completely
     /// deduped can corrupt this structure's internal storage
-    pub unsafe fn push(&mut self, item: V) -> InsertPosition {
-        while let Some(delta) = self.read_delta() {
-            let new_val = self.cur_val + delta.as_value();
+    pub unsafe fn push(&mut self, item: u64) -> InsertPosition {
+        while let Some((delta, size)) = self.read_delta() {
+            let new_val = self.cur_val + delta;
 
             if item < new_val {
                 break;
@@ -340,7 +156,9 @@ where
                 return InsertPosition::AlreadyExists(self.idx);
             }
 
-            self.advance(delta);
+            self.idx += 1;
+            self.pos += size;
+            self.cur_val += delta;
         }
 
         let delta = {
@@ -352,79 +170,58 @@ where
                 ..
             } = self;
 
-            let delta = Delta::<V, D>::compress(item - *cur_val);
+            let delta = item - *cur_val;
 
-            let _ = inner.splice(*pos..*pos, delta.to_bytes());
+            let encoded_delta = write_varint_to_buf(delta).unwrap();
+            let encoded_len = encoded_delta.len();
+
+            let _ = inner.splice(*pos..*pos, encoded_delta);
 
             *cur_val = item;
-            *pos += delta.size_of_val();
+            *pos += encoded_len;
             *idx += 1;
 
             delta
         };
 
-        if let Some(next_delta) = self.read_delta() {
+        if let Some((next_delta, size)) = self.read_delta() {
             let Self {
                 pos,
                 vec: PackedVec { inner, .. },
                 ..
             } = *self;
 
-            let new_delta = Delta::<V, D>::compress(next_delta.as_value() - delta.as_value());
+            let new_delta = next_delta - delta;
 
-            let _ = inner.splice(pos..pos + next_delta.size_of_val(), new_delta.to_bytes());
+            let _ = inner.splice(pos..pos + size, write_varint_to_buf(new_delta).unwrap());
         }
 
         InsertPosition::Position(self.idx - 1)
     }
 }
 
-impl<V, D> Extend<V> for PackedVec<V, D>
-where
-    V: Num + From<D>,
-    D: Num + TryFrom<V>,
-    [u8; size_of::<V>()]: ,
-    [u8; size_of::<D>()]: ,
-{
-    fn extend<T: IntoIterator<Item = V>>(&mut self, iter: T) {
+impl Extend<u64> for PackedVec {
+    fn extend<T: IntoIterator<Item = u64>>(&mut self, iter: T) {
         iter.into_iter().for_each(|el| {
             self.push(el);
         });
     }
 }
 
-pub struct PackedVecIter<'s, V, D>
-where
-    V: Num + From<D>,
-    D: Num + TryFrom<V>,
-    [u8; size_of::<V>()]: ,
-    [u8; size_of::<D>()]: ,
-{
-    raw: &'s [u8],
-    cur_val: V,
-
-    _marker: PhantomData<D>,
+pub struct PackedVecIter<'s> {
+    raw: io::Cursor<&'s [u8]>,
+    cur_val: u64,
 }
 
-impl<'s, V, D> Iterator for PackedVecIter<'s, V, D>
-where
-    V: Num + From<D>,
-    D: Num + TryFrom<V>,
-    [u8; size_of::<V>()]: ,
-    [u8; size_of::<D>()]: ,
-{
-    type Item = V;
+impl<'s> Iterator for PackedVecIter<'s> {
+    type Item = u64;
 
     fn next(&mut self) -> Option<Self::Item> {
-        if self.raw.is_empty() {
+        if self.raw.position() == self.raw.get_ref().len() as u64 {
             None
         } else {
-            let delta = read_delta::<V, D>(&self.raw);
-            self.raw = &self.raw[delta.size_of_val()..];
-
-            let delta = delta.as_value();
-
-            self.cur_val = self.cur_val + delta;
+            let delta = read_varint(&mut self.raw).unwrap();
+            self.cur_val += delta;
             Some(self.cur_val)
         }
     }
@@ -432,7 +229,7 @@ where
 
 #[cfg(test)]
 mod tests {
-    use std::{collections::HashSet, iter};
+    use std::{collections::HashSet, iter, mem::size_of};
 
     use rand::{thread_rng, Rng};
 
@@ -440,7 +237,7 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let mut vec = PackedVec::<u64, u8>::new();
+        let mut vec = PackedVec::new();
 
         let expected = [6345, 234, 34632, 34633, 656, 57];
 
@@ -497,7 +294,7 @@ mod tests {
     #[test]
     #[cfg_attr(debug_assertions, should_panic)]
     fn test_debug_assertion_deduped() {
-        let mut vec = PackedVec::<u64, u8>::new();
+        let mut vec = PackedVec::new();
 
         #[cfg(debug_assertions)]
         unsafe {
@@ -508,7 +305,7 @@ mod tests {
     #[test]
     #[cfg_attr(debug_assertions, should_panic)]
     fn test_debug_assertion_sorted() {
-        let mut vec = PackedVec::<u64, u8>::new();
+        let mut vec = PackedVec::new();
 
         #[cfg(debug_assertions)]
         unsafe {
@@ -518,12 +315,12 @@ mod tests {
 
     #[test]
     fn test_ratio() {
-        let mut vec = PackedVec::<u32, u8>::new();
+        let mut vec = PackedVec::new();
 
         let mut rng = thread_rng();
 
         let mut input = iter::from_fn(move || Some(rng.gen()))
-            .take(2_000_000)
+            .take(250_000)
             .collect::<Vec<_>>();
 
         input.sort_unstable();
@@ -533,7 +330,7 @@ mod tests {
             vec.extend_from_sorted_unique_slice(&input);
         }
 
-        println!("Original length: {}", input.len() * size_of::<u64>());
-        println!("u32 delta length: {}", vec.inner.len());
+        println!("Original len: {}", input.len() * size_of::<u64>());
+        println!("Packed len:   {}", vec.inner.len());
     }
 }
